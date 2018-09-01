e72feabd52647ec17011aa067089eea8b7b7f7ba
root <ali.boustani.ict@gmail.com>
2018-09-01 13:24:02 +0430

    Adding more Devices types(Juniper)

--- Using Netmiko/Backup-Netmiko.py
+++ Using Netmiko/Backup-Netmiko.py
@@ -7,15 +7,14 @@ from paramiko.ssh_exception import SSHException
 try:
     ans=True
     while ans:
-        print(""" Vendors List:  \n
+        print("""\n Vendors List:  \n
         1.Cisco
         2.Juniper
         3.Arista
         4.HP
-        5.Linux
-        6.Exit/Quite
+        5.Exit/Quite
         """)
-        ans=raw_input("Choose one of them!? ")
+        ans=raw_input(" Choose one of them!? ")
         if ans=="1":
             ans2=True
             while ans2:
@@ -31,28 +30,33 @@ try:
                 ans2=raw_input(" Choose one of the above mentioned device types? ")
                 if ans2=="1":
                     print("\n>>>>>You Choosed 'Cisco IOS' <<<<<'\n")
-                    devtype = "cisco_ios"
                     ans = False
+                    devtype = "cisco_ios"
+                    cmd = "show run"
                     break
                 elif ans2=="2":
                     print("\n>>>>>You Choosed 'Cisco IOS-XE' <<<<<'\n")
                     ans = False
                     devtype = "cisco_xe"
+                    cmd = "show run"
                     break
                 elif ans2=="3":
                     print("\n>>>>>You Choosed 'Cisco IOS-XR' <<<<<'\n")
                     ans = False
                     devtype = "cisco_xr"
+                    cmd = "show run"
                     break
                 elif ans2=="4":
                     print("\n>>>>>You Choosed 'Cisco NXOS' <<<<<'\n")
                     ans = False
                     devtype = "cisco_nxos"
+                    cmd = "show run"
                     break
                 elif ans2=="5":
                     print("\n>>>>>You Choosed 'Cisco ASA' <<<<<'\n")
                     ans = False
                     devtype = "cisco_asa"
+                    cmd = "show run"
                     ans = False
                     break
                 elif ans2=="6":
@@ -65,14 +69,17 @@ try:
                 else:
                     print("\n NOT Valid !!! Please Try again")
         elif ans=="2":
-            print("\n Juniper Devices does not supported yet")
+            print("\n>>>>>You Choosed 'Juniper' <<<<<'\n")
+            ans = False
+            devtype = "juniper"
+            cmd = "show configuration"
+            ans = False
+            break
         elif ans=="3":
             print("\n Arista Devices does not supported yet")
         elif ans=="4":
             print("\n Hp Devices does not supported yet") 
         elif ans=="5":
-            print("\n Linux Devices does not supported yet") 
-        elif ans=="6":
             print("\n Till next time") 
             ans = None
     
@@ -102,7 +109,7 @@ try:
         }
         try:
             net_connect = ConnectHandler(**device)
-            output = net_connect.send_command("show run")
+            output = net_connect.send_command(cmd)
             with open("DeviceIP__"+ ip, "w") as file:
                 file.write(output)
                 
