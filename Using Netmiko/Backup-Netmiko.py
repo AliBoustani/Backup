25ca80e25a198ca17eae50b53dbafdafbda5664d
root <ali.boustani.ict@gmail.com>
2018-09-01 12:42:13 +0430

    Adding more Devices types(Cisco models)

--- Using Netmiko/Backup-Netmiko.py
+++ Using Netmiko/Backup-Netmiko.py
@@ -5,13 +5,88 @@ from netmiko.ssh_exception import AuthenticationException
 from paramiko.ssh_exception import SSHException
 
 try:
-    username = raw_input("Please Enter Your SSH Usernmae : ")
+    ans=True
+    while ans:
+        print(""" Vendors List:  \n
+        1.Cisco
+        2.Juniper
+        3.Arista
+        4.HP
+        5.Linux
+        6.Exit/Quite
+        """)
+        ans=raw_input("Choose one of them!? ")
+        if ans=="1":
+            ans2=True
+            while ans2:
+                print("""\n>>>>> Which Cisco device do you have? \n
+                  1.Cisco IOS
+                  2.Cisco IOS-XE
+                  3.Cisco IOS-XR
+                  4.Cisco NX-OS
+                  5.Cisco ASA
+                  6.Back to Vendors
+                  7.Exit/Quit
+                  """)
+                ans2=raw_input(" Choose one of the above mentioned device types? ")
+                if ans2=="1":
+                    print("\n>>>>>You Choosed 'Cisco IOS' <<<<<'\n")
+                    devtype = "cisco_ios"
+                    ans = False
+                    break
+                elif ans2=="2":
+                    print("\n>>>>>You Choosed 'Cisco IOS-XE' <<<<<'\n")
+                    ans = False
+                    devtype = "cisco_xe"
+                    break
+                elif ans2=="3":
+                    print("\n>>>>>You Choosed 'Cisco IOS-XR' <<<<<'\n")
+                    ans = False
+                    devtype = "cisco_xr"
+                    break
+                elif ans2=="4":
+                    print("\n>>>>>You Choosed 'Cisco NXOS' <<<<<'\n")
+                    ans = False
+                    devtype = "cisco_nxos"
+                    break
+                elif ans2=="5":
+                    print("\n>>>>>You Choosed 'Cisco ASA' <<<<<'\n")
+                    ans = False
+                    devtype = "cisco_asa"
+                    ans = False
+                    break
+                elif ans2=="6":
+                    break
+                elif ans2=="7":
+                    print("\n Goodbye") 
+                    ans2 = False
+                    ans = None
+                    break
+                else:
+                    print("\n NOT Valid !!! Please Try again")
+        elif ans=="2":
+            print("\n Juniper Devices does not supported yet")
+        elif ans=="3":
+            print("\n Arista Devices does not supported yet")
+        elif ans=="4":
+            print("\n Hp Devices does not supported yet") 
+        elif ans=="5":
+            print("\n Linux Devices does not supported yet") 
+        elif ans=="6":
+            print("\n Till next time") 
+            ans = None
+    
+        else:
+            print("\n Not Valid Choice Try again")  
+
+
+    username = raw_input("\n Please Enter Your SSH Usernmae : ")
     password = getpass()
     
     print ". . . . . . . . . . . . \n"
     print ". . . . . . . . . . . . \n"
     
-    IPs = raw_input("Please Enter name of the file that contains IP addresses : ")
+    IPs = raw_input("\n Please Enter name of the file that contains IP addresses : ")
     
     with open(IPs) as f:
         myIPs = f.read().splitlines()
@@ -19,14 +94,14 @@ try:
     for IP in myIPs:
         print "Establishing Connection to . .  " + IP
         ip = IP
-        ios_device = {
-            "device_type": "cisco_ios",
+        device = {
+            "device_type": devtype,
             "ip": ip,
             "username": username,
             "password": password
         }
         try:
-            net_connect = ConnectHandler(**ios_device)
+            net_connect = ConnectHandler(**device)
             output = net_connect.send_command("show run")
             with open("DeviceIP__"+ ip, "w") as file:
                 file.write(output)
